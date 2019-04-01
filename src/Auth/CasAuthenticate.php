26f8da92d631d3c790664ad54ebd5509fbbfb0ce
Averill Cate, Jr <acate@email.arizona.edu>
2019-04-01 13:28:34 -0700

    Added CasAuthenticate based on work in Auth project.

--- src/Auth/CasAuthenticate.php
+++ src/Auth/CasAuthenticate.php
@@ -1,23 +1,6 @@
 <?php
-/**
- * Copyright 2015 Glen Sawyer
 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @copyright 2015 Glen Sawyer
- * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
- */
-namespace CasAuth\Auth;
+namespace App\Auth;
 
 use Cake\Auth\BaseAuthenticate;
 use Cake\Controller\ComponentRegistry;
@@ -37,129 +20,106 @@ class CasAuthenticate extends BaseAuthenticate
     protected $_defaultConfig = [
         'hostname' => null,
         'port' => 443,
-        'uri' => ''
+        'uri' => '',
+        'debug' => false,
+        'start_session' => false,
+        'cert_path' => '',
     ];
 
-    /**
-     * {@inheritDoc}
-     */
-    public function __construct(ComponentRegistry $registry, array $config = [])
+    public function __construct(ComponentRegistry $registry, $config = [])
     {
-        //Configuration params can be set via global Configure::write or via Auth->config
-        //Auth->config params override global Configure, so we'll pass them in last
-        parent::__construct($registry, (array)Configure::read('CAS'));
-        $this->setConfig($config);
 
-        //Get the merged config settings
+        // Set config params using Configure::write or Auth->config
+        parent::__construct($registry, (array)Configure::read('CAS'));
+
+        $this->setConfig($config);
         $settings = $this->getConfig();
 
+        // Set debug if specfied in config.
         if (!empty($settings['debug'])) {
-            phpCAS::setDebug(LOGS . 'phpCas.log');
+            phpCAS::setDebug(LOGS . 'phpcas.log');
         }
 
-        //The "isInitialized" check isn't necessary during normal use,
-        //but during *testing* if Authentication is tested more than once, then
-        //the fact that phpCAS uses a static global initialization can
-        //cause problems
         if (!phpCAS::isInitialized()) {
-            phpCAS::client(CAS_VERSION_2_0, $settings['hostname'], $settings['port'], $settings['uri']);
+            phpCAS::client(
+                CAS_VERSION_2_0,
+                $settings['hostname'],
+                $settings['port'],
+                $settings['uri'],
+                $settings['start_session']
+            );
         }
 
+        // No server validation for now, during dev.
         if (empty($settings['cert_path'])) {
-            phpCAS::setNoCasServerValidation();
+           phpCAS::setNoCasServerValidation();
         } else {
-            phpCAS::setCasServerCACert($settings['cert_path']);
+           phpCAS::setCasServerCACert($settings['cert_path']);
         }
 
         if (!empty($registry)) {
-            $controller = $registry->getController();
-            if (!empty($controller)) {
-                $this->setEventManager($controller->getEventManager());
-            }
+           $controller = $registry->getController();
+           if (!empty($controller)) {
+               $this->setEventManager($controller->getEventManager());
+           }
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public function authenticate(ServerRequest $request, Response $response)
     {
         phpCAS::handleLogoutRequests(false);
         phpCAS::forceAuthentication();
-        //If we get here, then phpCAS::forceAuthentication returned
-        //successfully and we are thus authenticated
 
         $user = array_merge(['username' => phpCAS::getUser()], phpCAS::getAttributes());
 
-        //Listen for this event if you need to add/modify CAS user attributes
         $event = $this->dispatchEvent('CasAuth.authenticate', $user);
         if (!empty($event->result)) {
             $user = $event->result;
         }
 
         return $user;
+
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public function getUser(ServerRequest $request)
     {
+
         if (empty($this->_registry)) {
             return false;
         }
+
         $controller = $this->_registry->getController();
         if (empty($controller->Auth)) {
             return false;
         }
 
-        //Since CAS authenticates externally (i.e. via browser redirect),
-        //we directly trigger Auth->identify() here
-        //This will eventually call back in to $this->authenticate above
-        $user = $controller->Auth->identify();
+        if (!phpCAS::isAuthenticated()) {
+            phpCAS::forceAuthentication();
+        }
 
-        $controller->Auth->setUser($user);
-
-        return $user;
+        $casUserName = phpCAS::getUser();
+        return ['userame' => $casUserName];
     }
 
-    /**
-     * Log a user out. Interrupts initial call to AuthComponent logout
-     * to handle CAS logout, which happens on separate CAS server
-     *
-     * @param Event $event Auth.logout event
-     *
-     * @return void
-     */
     public function logout(Event $event)
     {
+
         if (phpCAS::isAuthenticated()) {
-            //Step 1. When the client clicks logout, this will run.
-            //        phpCAS::logout will redirect the client to the CAS server.
-            //        The CAS server will, in turn, redirect the client back to
-            //        this same logout URL.
-            //
-            //        phpCAS will stop script execution after it sends the redirect
-            //        header, which is a problem because CakePHP still thinks the
-            //        user is logged in. See Step 2.
-            $auth = $event->subject();
+            $auth = $event->getSubject();
             if ($auth instanceof AuthComponent) {
-                $redirectUrl = $auth->config('logoutRedirect');
+                $redirectUrl = $auth->getConfig('logoutRedirect');
             }
+
             if (empty($redirectUrl)) {
                 $redirectUrl = '/';
             }
-            phpCAS::logout(['url' => Router::url($redirectUrl, true)]);
         }
-        //Step 2. We reach this line when the CAS server has redirected the
-        //        client back to us. Do nothing in this block; then after this
-        //        method returns, CakePHP will do whatever is necessary to log
-        //        the user out from its end (destroying the session or whatever).
+
+        $logoutUrl = env('CAS_LOGOUT_URL', 'https://sumeria.private.grad.arizona.edu');
+        phpCAS::logout(['url' => $logoutUrl]);
+
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public function implementedEvents()
     {
         return ['Auth.logout' => 'logout'];
