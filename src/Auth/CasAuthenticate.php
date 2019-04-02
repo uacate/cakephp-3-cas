ba2c9de910dcba9f87853132d3080e3ff6f932b4
Averill Cate, Jr <acate@email.arizona.edu>
2019-04-02 09:39:02 -0700

    Modifications based on github feedback.

--- src/Auth/CasAuthenticate.php
+++ src/Auth/CasAuthenticate.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Copyright 2015 Glen Sawyer
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright 2015 Glen Sawyer
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
 
-namespace App\Auth;
+namespace CasAuth\Auth;
 
 use Cake\Auth\BaseAuthenticate;
 use Cake\Controller\ComponentRegistry;
@@ -115,7 +132,10 @@ class CasAuthenticate extends BaseAuthenticate
             }
         }
 
-        $logoutUrl = env('CAS_LOGOUT_URL', 'https://sumeria.private.grad.arizona.edu');
+        $logoutUrl = getenv('CAS_LOGOUT_URL');
+        if ($logoutUrl == FALSE) {
+            throw new Exception("No CAS logout url provided.");
+        }
         phpCAS::logout(['url' => $logoutUrl]);
 
     }
